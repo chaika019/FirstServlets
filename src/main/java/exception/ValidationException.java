94ad6b14d37cd5481224990258dc22d62a930239
Chuiko Egor <egorchuiko7@gmail.com>
2025-09-30 02:46:41 +0300

    конструкторы для ошибок, List.copyOf() создает неизменяемую копию списка

--- src/main/java/exception/ValidationException.java
+++ src/main/java/exception/ValidationException.java
@@ -4,13 +4,24 @@ import lombok.Getter;
 import validator.Error;
 
 import java.util.List;
+import java.util.Collections;
 
+@Getter
 public class ValidationException extends RuntimeException {
 
-    @Getter
     private final List<Error> errors;
 
     public ValidationException(List<Error> errors) {
-        this.errors = errors;
+        this.errors = errors != null ? List.copyOf(errors) : Collections.emptyList();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+        this.errors = List.of(Error.of("validation.error", message));
+    }
+
+    public ValidationException(String code, String message) {
+        super(message);
+        this.errors = List.of(Error.of(code, message));
     }
 }
