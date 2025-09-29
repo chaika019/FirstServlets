852a211b85dbb68bcba0144e80210a5c7e3a600a
Chuiko Egor <egorchuiko7@gmail.com>
2025-09-30 02:41:50 +0300

    Bсправлен билдер с гендером +проверка

--- src/main/java/mapper/CreateUserMapper.java
+++ src/main/java/mapper/CreateUserMapper.java
@@ -4,24 +4,28 @@ import dto.CreateUserDto;
 import entity.User;
 import entity.Gender;
 import entity.Role;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import utils.LocalDateFormatter;
 
-import static lombok.AccessLevel.PRIVATE;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreateUserMapper implements Mapper<User, CreateUserDto> {
 
-@AllArgsConstructor(access = PRIVATE)
-public class CreateUserMapper implements Mapper<User, CreateUserDto>{
     private static final CreateUserMapper INSTANCE = new CreateUserMapper();
 
     @Override
     public User mapFrom(CreateUserDto object) {
+        if (object == null) {
+            throw new IllegalArgumentException("CreateUserDto cannot be null");
+        }
+
         return User.builder()
                 .name(object.getName())
                 .birthday(LocalDateFormatter.format(object.getBirthday()))
                 .email(object.getEmail())
                 .password(object.getPassword())
                 .role(Role.valueOf(object.getRole()))
-                .gender(Gender.valueOf(object.getRole()))
+                .gender(Gender.valueOf(object.getGender()))
                 .build();
     }
 
